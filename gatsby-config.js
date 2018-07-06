cef6b9b722d07613519ed01b13ac2c6fe1f5106d
Paul Lynch <plynch@corp.nextdigital.com>
2018-07-06 16:26:46 +1000

    Adding netlify CMS plugin

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Depression Boy`,
+    siteUrl: `http://artofpkl.com`,
+    description: `Depression Boy site (jam stack dev)`,
+  },
+  plugins: [`gatsby-plugin-netlify-cms`]
+};
