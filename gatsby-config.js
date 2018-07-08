4b78ee644b61da7990d7b8e4a8082d8cb4ebf1e8
A-A-Z <dramatichamster@gmail.com>
2018-07-08 22:56:07 +1000

    Updates to basics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,15 @@ module.exports = {
     siteUrl: `http://artofpkl.com`,
     description: `Depression Boy site (jam stack dev)`,
   },
-  plugins: [`gatsby-plugin-netlify-cms`]
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/issues`,
+        name: 'issues',
+      },
+    },
+    'gatsby-transformer-remark'
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,49 @@
 import React from "react"
+import PropTypes from 'prop-types'
 
 export default class IndexPage extends React.Component {
   render() {
+    const { data } = this.props
+    const { edges: issues } = data.allMarkdownRemark
+    console.log(data, issues)
+
     return (
-      <div>Depression Boy</div>
+      <div>
+        <h1>Depression Boy</h1>
+        <ul style={{ display: 'none' }}>
+          {issues.map(({ node: issue }) => {
+            const title = issue.frontmatter.title
+            return (
+              <li key={`isuee-${title}`}>{title}</li>
+            )
+          })}
+        </ul>
+      </div>
     )
   }
 }
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___title] },
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }`
