b00524d4d3c164bb4c76504cfcb5133636280982
Paul Lynch <plynch@corp.nextdigital.com>
2018-07-13 18:14:47 +1000

    Added helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,29 @@
 import React from "react"
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: issues } = data.allMarkdownRemark
+    const site = data.site.siteMetadata
 
     return (
       <div>
+        <Helmet
+          title={`${site.title}`}
+          meta={[
+            { name: 'description', content: site.description },
+            { name: 'keywords', content: 'depressionboy' },
+            { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+          ]}
+        />
         <h1>Depression Boy</h1>
         <ul>
           {issues.map(({ node: issue }) => {
             const title = issue.frontmatter.title
             return (
-              <li key={`isuee-${title}`}>{title}</li>
+              <li key={`isuee-${title}`}><a href={`/${issue.frontmatter.title}`}>{issue.frontmatter.title} | {issue.frontmatter.word}</a></li>
             )
           })}
         </ul>
@@ -32,6 +42,12 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
     allMarkdownRemark(
       sort: { order: ASC, fields: [frontmatter___title] },
     ) {
@@ -41,6 +57,7 @@ export const pageQuery = graphql`
           id
           frontmatter {
             title
+            word
           }
         }
       }
--- src/templates/issues.js
+++ src/templates/issues.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
 export default function Template({
   data
 }) {
-  const post = data.markdownRemark;
+  const post = data.markdownRemark
+  const site = data.site.siteMetadata
+  const keywords = post.frontmatter.tags.join(', ')
+
   return (
     <div>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-      </div>
+      <Helmet
+        title={`${site.title} : ${post.frontmatter.title} | ${post.frontmatter.word}`}
+        meta={[
+          { name: 'description', content: site.description },
+          { name: 'keywords', content: keywords },
+          { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+        ]}
+      />
+      <h1>{post.frontmatter.title} | {post.frontmatter.word}</h1>
     </div>
   )
 }
 
 export const pageQuery = graphql`
   query IssueByTitle($title: String!) {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
     markdownRemark(frontmatter: { title: { eq: $title } }) {
       id
       frontmatter {
         title
+        word
+        tags
       }
     }
   }
